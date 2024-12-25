792495fc4c08497e563ab204799879b50e4316b4
Fuznn <salangsangaaron905@gmail.com>
2024-12-25 15:31:14 +0800

    Initial commit

---
+++ src/main/java/net/fuzn/primamod/PrimaModClient.java
@@ -0,0 +1,11 @@
+package net.fuzn.primamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrimaModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
